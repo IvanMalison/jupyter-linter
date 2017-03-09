0c0442921427a707453ac3611e6681da100c96c7
yinleon <hello.leonyin@gmail.com>
2017-03-09 15:53:48 -0800

    got backend working

--- jupyter_linter/__init__.py
+++ jupyter_linter/__init__.py
@@ -5,7 +5,6 @@ import subprocess
 from notebook.utils import url_path_join
 from notebook.base.handlers import IPythonHandler
 
-
 def load_jupyter_server_extension(nb_server_app):
     """Called when the extension is loaded.
 
@@ -13,7 +12,7 @@ def load_jupyter_server_extension(nb_server_app):
         webserver instance.
 
     """
-    print "YAY it works"
+    print("YAY it works")
     web_app = nb_server_app.web_app
     host_pattern = '.*$'
     route_pattern = url_path_join(web_app.settings['base_url'], '/lint')
@@ -24,10 +23,17 @@ class LinterHandler(IPythonHandler):
 
     def post(self):
         request_dict = json.loads(self.request.body)
-        f, file_path = tempfile.mkstemp()
-        f.write(request_dict['to_lint'])
-        f.close()
+        f = tempfile.NamedTemporaryFile(delete=False)
 
-        linted = subprocess.check_output(["flake8", file_path])
+        print(request_dict)
+
+        with f as f_object:
+            f_object.write(request_dict['to_lint'].encode('utf-8'))
+
+        process = subprocess.Popen(["flake8", f.name], stdout=subprocess.PIPE)
+
+        print("done")
+        linted = process.stdout.read()
 
         print(linted)
+        self.finish(linted)
